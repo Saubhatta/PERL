a40d7c64c4383e4d7d93c2fec3c3350a135d5d58
Saubhatta <saubhatta.90@gmail.com>
2018-08-03 14:49:53 +0530

    Add existing file

---
+++ program2_7.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+print ("What is 17 plus 26?\n");
+
+$correct_answer = 43; 		#The correct answer
+
+$input_answer = <STDIN>;
+
+chop ($input_answer);
+
+until ($input_answer == $correct_answer) {
+
+	print ("Wrong! keep trying!\n");
+	
+	$input_answer = <STDIN>;
+	
+	chop ($input_answer);
+
+}
+
+print ("You've got it!\n");
