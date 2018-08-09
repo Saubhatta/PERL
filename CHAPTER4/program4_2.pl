5fdb9b7510f4c97e73c0b2fccc09fbdc88f74a02
Saubhatta <saubhatta.90@gmail.com>
2018-08-09 12:20:03 +0530

    New exercises

---
+++ CHAPTER4/program4_2.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+$value1 = 14.3;
+
+$value2 = 100 + 14.3 - 100;
+
+print("Value1 is $value1, Value2 is $value2\n");
+
+if ($value1 == $value2) {
+
+        print("value 1 equals value 2\n");
+
+
+} else {
+
+        print("value 1 does not equal value 2\n");
+
+}
---
+++ CHAPTER4/program4_4.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+$value = 0;
+
+while ($value++ <= 5) {
+
+        print("value is now $value\n");
+
+}
+
+print("all done\n");
