d30817d2b0bc53db7c5b0024ce9f543ce4d453d6
Saubhatta <saubhatta.90@gmail.com>
2018-10-04 16:54:31 +0530

    Adding files

---
+++ CHAPTER5/program5_1.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl                                   
+
+@array = (1, "chicken", 1.23, "\"Having fun?\"", 9.33e+23);
+
+$count = 1;
+
+while ($count <= 5) {
+
+        print ("element $count is $array[$count-1]\n");
+
+                $count++;
+
+                }
